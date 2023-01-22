24bddc7d232e2dda929c815e988fcda75eaa84cf
Your Name <pavlo.morozenko@ukr.net>
2023-01-22 12:19:47 +0100

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredList = document.querySelector("#ingredients");
+const createEl = (text) => {
+  const ingredList = document.createElement("li");
+  ingredList.textContent = text;
+  ingredList.classList.add("item");
+  return ingredList;
+};
+
+const addItems = (array) => {
+  ingredList.append(...array.map((element) => createEl(element)));
+};
+addItems(ingredients);
