88914fcf0e1e4884a449c5452a209de1184da68d
Your Name <pavlo.morozenko@ukr.net>
2023-01-25 13:54:05 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,17 @@
-const inputRangeRef = document.querySelector("#font-size-control");
-const inputTextRef = document.querySelector("#text");
-const handleInput = () => {
-  inputTextRef.style.fontSize = `${inputRangeRef.value}px`;
-};
-inputRangeRef.addEventListener("input", handleInput);
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  form.reset();
+  if (email.length === 0 || password.length === 0) {
+    alert("All fields must be completed!");
+  }
+}
