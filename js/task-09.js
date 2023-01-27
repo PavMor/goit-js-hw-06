964c7c8f568e53a4155442774dd8480780e166f8
Your Name <pavlo.morozenko@ukr.net>
2023-01-27 19:56:35 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = randomColor();
+});
+
+const randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
