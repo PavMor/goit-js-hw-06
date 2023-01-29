01ceee0268ac8196b4751b50b287545a4fe220c9
Your Name <pavlo.morozenko@ukr.net>
2023-01-29 12:23:10 +0100

    task-9.3

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const changeColorBtn = document.querySelector(".change-color");
-const colorName = document.querySelector(".color");
-changeColorBtn.addEventListener("click", onChangeColorBtnClick);
+const changeColorBtnRef = document.querySelector(".change-color");
+const colorNameRef = document.querySelector(".color");
+changeColorBtnRef.addEventListener("click", onChangeColorBtnClick);
 function onChangeColorBtnClick() {
   document.body.style.backgroundColor = getRandomHexColor();
-  colorName.innerHTML = document.body.style.backgroundColor;
+  colorNameRef.innerHTML = document.body.style.backgroundColor;
 }
